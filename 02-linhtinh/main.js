fa68265afe9b33ff91196d37902d93e43ea8e52f
NhaPhuowng <phuongptn03@gmail.com>
2025-06-17 10:44:27 +0700

    binding attribute

--- 02-linhtinh/main.js
+++ 02-linhtinh/main.js
@@ -1,16 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
+        urlTest: 'https://vuejs.org/',
+        classTest: 'text-red',
         products: [
             {
+                thumb:"./assets/p1.png",
                 name: 'Product 1',
                 price: 100,
             },
             {
+                thumb:"./assets/p2.png",
                 name: 'Product 2',
                 price: 100,
             },
             {
+                thumb:"./assets/p3.png",
                 name: 'Product 3',
                 price: 100,
             },
